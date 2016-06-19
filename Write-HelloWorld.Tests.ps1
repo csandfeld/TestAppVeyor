cfd27c07d1121696d06d4b6292cfa8183b9e14b0
csandfeld <csandfeld@users.noreply.github.com>
2016-06-19 11:37:24 +0200

    Initial commit

---
+++ Write-HelloWorld.Tests.ps1
@@ -0,0 +1,10 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace('.Tests.', '.')
+. "$here\$sut"
+
+Describe 'Write-HelloWorld' {
+    It 'The function should exist' {
+        Get-Command -Name Write-HelloWorld -ErrorAction SilentlyContinue |
+        Should Be $true 
+    }
+}
